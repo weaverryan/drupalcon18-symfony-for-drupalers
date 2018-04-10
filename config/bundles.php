88a43f1e951fd6d522c127a6be2ab59e2902a3b1
Ryan Weaver <ryan@thatsquality.com>
2018-04-10 15:19:37 -0400

    Step 21: API: Install API Platform

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,7 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
