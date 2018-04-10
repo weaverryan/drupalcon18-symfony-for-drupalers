4022d49574b6ea4a4978322c2e04bb9dd8103546
Ryan Weaver <ryan@thatsquality.com>
2018-04-10 15:19:34 -0400

    Step 15: Doctrine: API endpoint to create song

--- src/Controller/SongController.php
+++ src/Controller/SongController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\CountrySong;
 use App\Service\SongGenerator;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,12 +12,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class SongController extends AbstractController
 {
     /**
-     * @Route("/api/songs")
+     * @Route("/api/songs", methods="POST")
      */
-    public function apiWriteSong(SongGenerator $songGenerator)
+    public function apiWriteSong(SongGenerator $songGenerator, EntityManagerInterface $em)
     {
+        $song = new CountrySong();
+        $song->setTitle($songGenerator->generateSong('truck'));
+        $em->persist($song);
+        $em->flush();
+
         return $this->json([
-            $songGenerator->generateSong('truck'),
+            'title' => $song->getTitle(),
+            'id' => $song->getId(),
         ]);
     }
 
